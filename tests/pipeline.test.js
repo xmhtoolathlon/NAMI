a7a6e69afab63df15f5cd111cbbdd11776749703
xmhtoolathlon <ygytyygtftytdrfcvgfrteseswaqqw@gmail.com>
2026-02-08 01:49:32 +0800

    Add tests/pipeline.test.js

---
+++ tests/pipeline.test.js
@@ -0,0 +1,16 @@
+/**
+ * Pipeline Tests
+ */
+const Pipeline = require('../nami/core/pipeline');
+
+describe('Pipeline', () => {
+    it('should execute stages in order', async () => {
+        const pipeline = new Pipeline({});
+        // Test implementation
+    });
+
+    it('should handle stage errors', async () => {
+        // FIXME: Add integration tests for error scenarios
+        // Need more comprehensive error testing
+    });
+});
